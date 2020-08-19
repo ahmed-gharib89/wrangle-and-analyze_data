bf71587b6775de0d1d6af62052a67592f0994cb5
Ahmed Gharib <64174723+ahmed-gharib89@users.noreply.github.com>
2020-08-19 03:13:53 +0200

    feat: add function to to get the twitter data to use it in the main jupyter notebook for this project to be able to hide the credintials for twitter API

--- twitter-api.py
+++ twitter-api.py
@@ -10,39 +10,44 @@ consumer_secret = 'HIDDEN'
 access_token = 'HIDDEN'
 access_secret = 'HIDDEN'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+# function to get the data from twitter api
+def get_twitter_data(df, file_path):
+    """
+    get_twitter_data: function to get twitter data using tweepy library
+    to access twitter API.
+    ARGS:
+        df: a pandas data frame which stores the tweets ids in tweet_id column.
+        file_path: a file path for the file to save the retrived data in.
+    Return: nothing
+    """
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# NOTE TO STUDENT WITH MOBILE VERIFICATION ISSUES:
-# df_1 is a DataFrame with the twitter_archive_enhanced.csv file. You may have to
-# change line 17 to match the name of your DataFrame with twitter_archive_enhanced.csv
-# NOTE TO REVIEWER: this student had mobile verification issues so the following
-# Twitter API code was sent to this student from a Udacity instructor
-# Tweet IDs for which to gather additional data via Twitter's API
-tweet_ids = df_1.tweet_id.values
-len(tweet_ids)
+    # Tweet IDs for which to gather additional data via Twitter's API
+    tweet_ids = df.tweet_id.values
+    len(tweet_ids)
 
-# Query Twitter's API for JSON data for each tweet ID in the Twitter archive
-count = 0
-fails_dict = {}
-start = timer()
-# Save each tweet's returned JSON as a new line in a .txt file
-with open('tweet_json.txt', 'w') as outfile:
-    # This loop will likely take 20-30 minutes to run because of Twitter's rate limit
-    for tweet_id in tweet_ids:
-        count += 1
-        print(str(count) + ": " + str(tweet_id))
-        try:
-            tweet = api.get_status(tweet_id, tweet_mode='extended')
-            print("Success")
-            json.dump(tweet._json, outfile)
-            outfile.write('\n')
-        except tweepy.TweepError as e:
-            print("Fail")
-            fails_dict[tweet_id] = e
-            pass
-end = timer()
-print(end - start)
-print(fails_dict)
+    # Query Twitter's API for JSON data for each tweet ID in the Twitter archive
+    count = 0
+    fails_dict = {}
+    start = timer()
+    # Save each tweet's returned JSON as a new line in a .txt file
+    with open(file_path, 'w') as outfile:
+        # This loop will likely take 20-30 minutes to run because of Twitter's rate limit
+        for tweet_id in tweet_ids:
+            count += 1
+            print(str(count) + ": " + str(tweet_id))
+            try:
+                tweet = api.get_status(tweet_id, tweet_mode='extended')
+                print("Success")
+                json.dump(tweet._json, outfile)
+                outfile.write('\n')
+            except tweepy.TweepError as e:
+                print("Fail")
+                fails_dict[tweet_id] = e
+                pass
+    end = timer()
+    print(end - start)
+    print(fails_dict)
